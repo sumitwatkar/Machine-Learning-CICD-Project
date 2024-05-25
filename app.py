2d3a13e713baa0757ecaa0271dc5e4ea25e61b80
Sumit Watkar <watkar.sumit@gmail.com>
2024-05-25 18:07:33 +0530

    cicd completed

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run(host="0.0.0.0", port=8080) #for deployment run
+    #app.run(host="127.0.0.1", port=8080,debug=True) # for local run
